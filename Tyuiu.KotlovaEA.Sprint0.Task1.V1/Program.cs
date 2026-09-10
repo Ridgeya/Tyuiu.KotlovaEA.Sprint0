51a493fac368049734447255434b82c86d9c5a8f
li <lizaadga@gmail.com>
2026-09-10 22:58:15 +0500

    Task1

---
+++ Tyuiu.KotlovaEA.Sprint0.Task1.V1/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Елизавета";
+        int age = 21;
+        Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " год.");
+        Console.ReadKey();
+
+    }
+}
