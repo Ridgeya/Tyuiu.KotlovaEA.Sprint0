15b52fdc7751c48a3eda6ed1a107d5aac0144816
li <lizaadga@gmail.com>
2026-09-12 14:03:31 +0500

    Task3

---
+++ Tyuiu.KotlovaEA.Sprint0.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.KotlovaEA.Sprint0.Task3.V1.Lib;
+namespace Tyuiu.KotlovaEA.Sprint0.Task3.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
---
+++ Tyuiu.KotlovaEA.Sprint0.Task3.V1/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.KotlovaEA.Sprint0.Task3.V1.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Sum(7, 7));
+        Console.ReadKey();
+    }
+}
