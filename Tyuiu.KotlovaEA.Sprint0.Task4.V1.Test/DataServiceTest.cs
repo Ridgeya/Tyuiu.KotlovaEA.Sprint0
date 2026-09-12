017d95f927b5595d52ecce092ce738714b25ad07
li <lizaadga@gmail.com>
2026-09-12 14:36:26 +0500

    Task4

---
+++ Tyuiu.KotlovaEA.Sprint0.Task4.V1.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Tyuiu.KotlovaEA.Sprint0.Task4.V1.Lib;
+namespace Tyuiu.KotlovaEA.Sprint0.Task4.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        public void ChekedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        public void ChekedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
