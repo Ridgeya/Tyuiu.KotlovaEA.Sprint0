40cd380963f3918eb9ee6df83eb5b5330fd6742f
li <lizaadga@gmail.com>
2026-09-15 20:19:13 +0500

    Task6

--- Tyuiu.KotlovaEA.Sprint0.Task6.V1.Lib/DataService.cs
+++ Tyuiu.KotlovaEA.Sprint0.Task6.V1.Lib/DataService.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.KotlovaEA.Sprint0.Task6.V1.Lib
     {
         public static object AdditionArray(int[] numbers)
         {
-            var total = 1;
+            var total = 0;
             for (var i = 0; i < numbers.Length; i++)
             {
                 total = total + numbers[i];
