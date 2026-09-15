395717d6b5ddc612c85cee5ea35fb9f37e40ab7c
li <lizaadga@gmail.com>
2026-09-15 18:36:41 +0500

    Task5

---
+++ Tyuiu.KotlovaEA.Sprint0.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.KotlovaEA.Sprint0.Task5.V1.Lib;
+namespace Tyuiu.KotlovaEA.Sprint0.Task5.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        public void ChekedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        public void ChekedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
